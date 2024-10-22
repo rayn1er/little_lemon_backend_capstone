0e3c106d29e7e0336f7409e8b109d20c6e16aec4
rayn1er <raulortizaray@gmail.com>
2024-10-22 12:09:48 -0400

    created models

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -76,11 +76,17 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'valkyrionsql..',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+           'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    inventory = models.IntegerField(null=False)
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    guest_number = models.IntegerField(null=False)
+    booking_date = models.DateTimeField()
