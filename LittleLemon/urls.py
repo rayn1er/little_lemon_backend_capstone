85caaf1873d554ba5496dc02818f1ff6026cd8d1
rayn1er <raulortizaray@gmail.com>
2024-10-22 14:03:57 -0400

    creating serializers

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from rest_framework import routers
+from Restaurant import views
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/', include('Restaurant.urls'))
+    path('restaurant/', include('Restaurant.urls')),
+    path('restaurant/menu/',include('Restaurant.urls')),
+    path('restaurant/booking/',include(router.urls))
 ]
+
